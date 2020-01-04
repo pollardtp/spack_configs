eee3e70492f63b57d692a7ef82174c0bf1452404
Travis P. Pollard <pollartpuc@gmail.com>
2020-01-03 19:05:57 -0500

    mod file

--- centennial/modspack_func.sh
+++ centennial/modspack_func.sh
@@ -1,3 +1,3 @@
 # Spack | default profile add to BASHRC or BASH_PROFILE
-alias modspack='module purge; module load mpt/2.20 gcc/8.2.0'
+alias modspack='module purge; module load mpi/sgimpt/2.18 compiler/gcc/7.3.0'
 
